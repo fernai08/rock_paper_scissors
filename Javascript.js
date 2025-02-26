7ab66f79c0170e279d4f77c649d0c04a29e9d119
Fernan Mejuto <103956824+fernai08@users.noreply.github.com>
2025-02-26 18:06:39 +0100

    Completed the rock, paper, scissors game and cleaned the Javascript file. Added indication before each function.

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,97 @@
-let randomNumber = Math.floor(Math.random());
-let rock = 1 ;
-let paper = 2 ;
-let scissors = 3 ;  
+// Variables//
+let rock = "rock" ;
+let paper = "paper";
+let scissors = "scissors";  
+let humanScore = 0 ;
+let computerScore = 0 ;
 
-function getComputerChoice () {
-  
-   
-    
-
+// Function getComputerChoice //
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return rock;
+  } else if (randomNumber === 1) {
+    return paper;
+  } else {
+    return scissors;
+  }
 }
-console.log(rock);
+
+
+// Function getHumanChoice //
+function getHumanChoice() {
+  let userChoice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+  
+  if (userChoice === rock || userChoice === paper || userChoice === scissors) {
+    return userChoice;
+  } else {
+    alert("Invalid Input, try again!");
+    return getHumanChoice(); // Ask again until valid input
+  }
+}
+
+// Function playRound, to determine the winner of the round and return the score // 
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`You chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    (humanChoice === rock && computerChoice === scissors) ||
+    (humanChoice === paper && computerChoice === rock) ||
+    (humanChoice === scissors && computerChoice === paper)
+  ) {
+    humanScore++;
+    return "You win this round!";
+  } else {
+    computerScore++;
+    return "You lose this round!";
+  }
+}
+// Function playGame , five rounds will be played and in each round we will collect humanChoice and computerChoice //
+
+function playGame () {
+ // Round 1 //
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+  console.log(playRound(humanChoice, computerChoice));
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore);
+
+  // Round 2 //
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log(playRound(humanChoice, computerChoice));
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore);  
+  
+  // Round 3 //
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log(playRound(humanChoice, computerChoice));
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore); 
+
+  // Round 4 //
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log(playRound(humanChoice, computerChoice));
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore); 
+
+  // Round 5 //
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log(playRound(humanChoice, computerChoice));
+  console.log("Human Score: " + humanScore, "Computer Score: " + computerScore); 
+
+  if (humanScore > computerScore) {
+    return "you win!!";
+  }
+  else if (humanScore < computerScore) {
+    return "You Loose!!";
+  }
+  else if (humanScore === computerScore) {
+    return "It's a tie!!";
+  }
+}
+  
+console.log(playGame());
